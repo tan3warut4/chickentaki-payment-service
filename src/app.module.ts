d0c95ba3d36cae548f0a8e2c4500c3b892c820fd
tan3warut4 <itanprscritical@gmail.com>
2025-06-05 21:21:10 +0700

    Add new package and implement  consumer

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,23 @@ import { AppService } from './app.service';
 import { LineModule } from './line/line.module';
 import { PaymentModule } from './payment/payment.module';
 import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PAYMENT_SERVICE_RABBITMQ } from './constant';
 
 
 @Module({
-  imports: [LineModule, PaymentModule,ConfigModule.forRoot()],
+  imports: [LineModule, PaymentModule, ClientsModule.register([{
+    name: PAYMENT_SERVICE_RABBITMQ,
+    transport: Transport.RMQ,
+    options:{
+      urls:["amqp://chickentaki:chickentaki@localhost:5672"],
+      queue:"payment_queue",
+      queueOptions:{
+        durable: true,
+      }
+    }
+  }]), ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
